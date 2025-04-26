5ee2e7b0e6ed433051ae629fadd7dbdfbcab6d8f
siddhiGvc2 <sd@gvc.in>
2025-04-26 10:36:28 +0530

    CASHRECEIVED COMMAND ADDED

--- main/src/mqttRoutines.c
+++ main/src/mqttRoutines.c
@@ -589,6 +589,11 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
                     sprintf(payload, "*QR-OK,%s#",QrString); 
                     publish_message(payload,client);
                 }
+                else if(strncmp(data,"*CASHRECEIVED,",14)==0)
+                {
+                    uart_write_string_ln(data);
+                    publish_message("CASHRECEIVED-OK",client);
+                }
                 else {
                     ESP_LOGI(TAG, "Unknown message received.");
                 }
